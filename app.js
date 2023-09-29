033a82367d8b882ccaa349c205a946caca6f75b7
fatimaismail180 <145142834+fatimaismail180@users.noreply.github.com>
2023-09-29 22:14:19 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,36 @@
+var userName="fatima";
+alert(userName);
+
+var myName="fatima "+"Ismail";
+alert(myName);
+
+var js;
+js="Hello World";
+alert(js);
+
+var name="Jhone Doe";
+alert(name);
+
+var aGe="15 yraes old";
+alert(aGe);
+
+var qualiFaction="Certified Mobile Application Development";
+alert(qualiFaction);
+
+var x=
+x="PIZZA\n PIZZ\n PIZ\n PI\n P";
+alert(x);
+
+var eMail=
+eMail="My email address is " + "fatimaismail180@gmail.com";
+alert(eMail);
+
+var bOok=
+bOok="A Smarter way to learn JavaScript";
+alert(bOok);
+
+ var z="yah!I Can write HTML content through JavaScript"
+ document.write(z);
+
+var y=("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”");
+alert(y);
